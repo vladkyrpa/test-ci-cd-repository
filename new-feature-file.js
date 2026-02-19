b8990d8b8464bbc599ca2ce01c2408931bc0b469
vladkyrpa <vlad.kyrpa@gmail.com>
2026-02-19 15:48:28 +0200

    fix: New-feature-file.js

--- new-feature-file.js
+++ new-feature-file.js
@@ -1,5 +1,9 @@
 console.log("some new feature");
 <<<<<<< HEAD
+<<<<<<< HEAD
 console.log("fix new feature");
 =======
 >>>>>>> 938d956 (feat: Added new feature file)
+=======
+console.log("fix new feature");
+>>>>>>> 5e28aa5 (fix: New-feature-file.js)
